cf0b2692276651da9039004f412595d8fa1fa628
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-01 18:56:47 +0000

    Add sliding window

--- Tests2021/Tests.fs
+++ Tests2021/Tests.fs
@@ -1,9 +1,19 @@
 module Tests
 
 open System
+open System.Collections
 open System.IO
 open Xunit
 open Xunit.Abstractions
+open FluentAssertions
+
+
+/// structurally compares two enumerables
+let shouldBeEquivalentsTo expected (actual: 'a :> IEnumerable) =
+    actual
+        .Should()
+        .BeEquivalentTo(expected :> IEnumerable, "")
+    |> ignore
 
 let IsDeeper a b = b > a
 
@@ -19,25 +29,44 @@ let rec Sweep (last: Option<int>) (rest: List<int>) : int =
              0)
         + Sweep next rest.Tail
 
-let SonarSweep depths : int = Sweep None depths
+let CountIncreases depths : int = Sweep None depths
+
+let rec SlidingWindow (output: ITestOutputHelper) (depths: int list) =
+    (depths |> Seq.take 3 |> Seq.sum)
+    :: if depths.Length > 3 then
+           SlidingWindow output depths.Tail
+       else
+           []
+
+let testData =
+    [ 199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263 ]
 
 type Part1(output: ITestOutputHelper) =
+
     [<Fact>]
-    let ``My test`` () =
-        Assert.Equal(
-            7,
-            [ 199
-              200
-              208
-              210
-              200
-              207
-              240
-              269
-              260
-              263 ]
-            |> SonarSweep
-        )
+    let ``Increases are counted`` () =
+        Assert.Equal(7, testData |> CountIncreases)
+
+    [<Fact>]
+    let ``Calculate sliding window totals`` () =
+        (testData |> SlidingWindow output)
+        |> shouldBeEquivalentsTo [ 607
+                                   618
+                                   618
+                                   617
+                                   647
+                                   716
+                                   769
+                                   792 ]
 
     [<Fact>]
     let ``for reals`` () =
@@ -45,6 +74,6 @@ type Part1(output: ITestOutputHelper) =
             File.ReadAllLines "data/day1input.txt"
             |> Array.toList
             |> List.map Int32.Parse
-            |> SonarSweep
+            |> CountIncreases
 
         output.WriteLine(result.ToString())
