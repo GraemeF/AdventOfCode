c8a2db3720748e78bea678788ac52937f237489e
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-03 14:24:47 +0000

    Start adding Day 3

---
+++ Tests2021/Day3.fs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode
+
+open System
+open System.IO
+open Helpers
+open Xunit
+open Xunit.Abstractions
+
+module Day3 =
+
+    let findGammaRate (diagnosticReport: seq<string>) =
+        let length = (diagnosticReport |> Seq.head).Length
+        length
+
+    type Tests(output: ITestOutputHelper) =
+
+        let testData =
+            "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"
+
+        [<Fact>]
+        let ``Finds gamma rate`` () =
+            Assert.Equal(22, testData.Split '\n' |> findGammaRate)
