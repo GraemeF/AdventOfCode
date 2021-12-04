451f01d24a7b908e558440f3f770a4401a9298a4
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-04 10:53:00 +0000

    Pass with test data

--- Tests2021/Day3.fs
+++ Tests2021/Day3.fs
@@ -1,16 +1,42 @@
 ﻿namespace AdventOfCode
 
-open System
-open System.IO
-open Helpers
 open Xunit
 open Xunit.Abstractions
 
 module Day3 =
 
+    let getBit (index: int) (line: string) =
+        match line.[index] with
+        | '0' -> false
+        | '1' -> true
+        | _ -> failwith "wtf is this"
+
+    let countBits index report =
+        Seq.map (getBit index) report
+        |> Seq.fold
+            (fun (zeros, ones) bit ->
+                if bit then
+                    (zeros, ones + 1)
+                else
+                    (zeros + 1, ones))
+            (0, 0)
+
+    let getDominantBits index diagnosticReport =
+        let zeros, ones = diagnosticReport |> countBits index
+        if zeros > ones then 0 else 1
+
+    let getBitValue index shift diagnosticReport =
+        (getDominantBits index diagnosticReport) <<< shift
+
     let findGammaRate (diagnosticReport: seq<string>) =
         let length = (diagnosticReport |> Seq.head).Length
-        length
+
+        [ 0 .. length - 1 ]
+        |> Seq.fold
+            (fun total index ->
+                total
+                + (getBitValue index (length - index - 1) diagnosticReport))
+            0
 
     type Tests(output: ITestOutputHelper) =
 
