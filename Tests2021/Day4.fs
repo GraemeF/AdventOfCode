6ce39fd693cee47476751756ef0dbf4329dfbdc4
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-05 18:26:11 +0000

    Calculate final score

--- Tests2021/Day4.fs
+++ Tests2021/Day4.fs
@@ -7,18 +7,26 @@ open Xunit.Abstractions
 
 module Day4 =
 
-    type Board = Option<int> [] []
+    type Number = { number: int; isMarked: bool }
+
+    type Board = Number [] []
 
     type BingoState =
         { drawnNumbers: seq<int>
+          lastCalledNumber: Option<int>
           boards: seq<Board> }
 
-    let parseBoardRow (input: string) : seq<Option<int>> =
+    let toUnmarkedNumber x = { number = x; isMarked = false }
+
+    let toBoard expected =
+        (expected |> Array.map (Array.map toUnmarkedNumber))
+
+    let parseBoardRow (input: string) : seq<Number> =
         input.Split ' '
         |> Seq.filter (fun s -> not (String.IsNullOrWhiteSpace s))
-        |> Seq.map (fun n -> Some(Convert.ToInt32 n))
+        |> Seq.map (fun n -> toUnmarkedNumber (Convert.ToInt32 n))
 
-    let parseBoard (input: seq<string>) : Option<int> [] [] =
+    let parseBoard (input: seq<string>) : Number [] [] =
         input
         |> Seq.map (fun line -> parseBoardRow line |> Seq.toArray)
         |> Seq.toArray
@@ -48,18 +56,36 @@ module Day4 =
         { drawnNumbers =
               (input |> Seq.head).Split ','
               |> Seq.map Convert.ToInt32
-          boards = input |> Seq.tail |> parseBoards }
+          boards = input |> Seq.tail |> parseBoards
+          lastCalledNumber = None }
 
-    let markRow (number: int) (row: Option<int> []) : Option<int> [] =
+    let markRow (number: int) (row: Number []) : Number [] =
         row
         |> Array.map
             (fun x ->
-                if (x.IsSome && x.Value = number) then
-                    None
+                if (x.number = number) then
+                    { x with isMarked = true }
                 else
                     x)
 
     let markBoard (number: int) (board: Board) : Board = board |> Array.map (markRow number)
+
+    let hasWinningRow board =
+        board
+        |> Seq.exists (Seq.forall (fun x -> x.isMarked))
+
+    let hasWinningColumn board =
+        let isWinningColumn index =
+            board
+            |> Seq.forall (fun (row: Number []) -> row.[index].isMarked)
+
+        let columnCount = board |> Seq.head |> Seq.length
+
+        [ 0 .. columnCount - 1 ]
+        |> Seq.exists isWinningColumn
+
+    let isWinner board =
+        (hasWinningRow board) || (hasWinningColumn board)
 
     let draw (state: BingoState) : BingoState =
         let drawnNumber = state.drawnNumbers |> Seq.head
@@ -68,9 +94,27 @@ module Day4 =
             state.boards |> Seq.map (markBoard drawnNumber)
 
         { drawnNumbers = state.drawnNumbers |> Seq.tail
-          boards = boards }
+          boards = boards
+          lastCalledNumber = Some drawnNumber }
 
-    let getFinalScore input = 0
+    let getWinningBoard state : Option<Board> = state.boards |> Seq.tryFind isWinner
+
+    let rec play (state: BingoState) : Board * int =
+        if getWinningBoard state = None then
+            play (draw state)
+        else
+            ((getWinningBoard state).Value, state.lastCalledNumber.Value)
+
+    let sumUnmarkedNumbers (board: Board) =
+        board
+        |> Seq.concat
+        |> Seq.filter (fun x -> not x.isMarked)
+        |> Seq.map (fun x -> x.number)
+        |> Seq.sum
+
+    let getFinalScore state : int =
+        let winningBoard, lastCalledNumber = play state
+        sumUnmarkedNumbers winningBoard * lastCalledNumber
 
     type Tests(output: ITestOutputHelper) =
 
@@ -132,7 +176,11 @@ module Day4 =
 
         [<Fact>]
         let ``Parses a board row`` () =
-            Assert.Equal(([| 22; 13; 17; 11; 0 |] |> Array.map Some), "22 13 17 11  0" |> parseBoardRow)
+            Assert.Equal(
+                ([| 22; 13; 17; 11; 0 |]
+                 |> Array.map toUnmarkedNumber),
+                ("22 13 17 11  0" |> parseBoardRow)
+            )
 
         [<Fact>]
         let ``Parses a board`` () =
@@ -151,11 +199,44 @@ module Day4 =
                    [| 6; 10; 3; 18; 5 |]
                    [| 1; 12; 20; 15; 19 |] |]
 
-            Assert.True(boardEqual (expected |> Array.map (Array.map Some)) board)
+            Assert.True(boardEqual (toBoard expected) board)
 
         [<Fact>]
         let ``Parses boards`` () =
             Assert.Equal(3, testData.boards |> Seq.length)
+
+        let marked = { number = 0; isMarked = true }
+        let unmarked = { number = 0; isMarked = false }
+        let unmarkedRow = [| unmarked; unmarked; unmarked |]
+        let winningRow = [| marked; marked; marked |]
+        let partlyMarkedRow = [| unmarked; marked; unmarked |]
+
+        [<Fact>]
+        let ``Returns true when board has a winning row`` () =
+            Assert.True(
+                [| unmarkedRow
+                   winningRow
+                   unmarkedRow |]
+                |> isWinner
+            )
+
+        [<Fact>]
+        let ``Returns true when board has a winning column`` () =
+            Assert.True(
+                [| partlyMarkedRow
+                   partlyMarkedRow
+                   partlyMarkedRow |]
+                |> isWinner
+            )
+
+        [<Fact>]
+        let ``Returns false when board has no winning rows or columns`` () =
+            Assert.False(
+                [| unmarkedRow
+                   unmarkedRow
+                   unmarkedRow |]
+                |> isWinner
+            )
 
         [<Fact>]
         let ``Calculates final score`` () =
