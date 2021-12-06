608655dd46915be6f24e0f38ab35ec3a06a4b99b
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-06 18:59:29 +0000

    Use counts of fish

--- Tests2021/Day6.fs
+++ Tests2021/Day6.fs
@@ -8,26 +8,24 @@ open Xunit.Abstractions
 module Day6 =
 
     type Lanternfish = int
-    type School = seq<Lanternfish>
+    type School = uint64 array
 
-    let simulateLanternfish (lanternfish: Lanternfish) : Lanternfish * Option<Lanternfish> =
+    let createSchool: School = Array.create 9 0UL
+
+    let simulateLanternfish (lanternfish: Lanternfish) : Lanternfish list =
         match lanternfish with
-        | 0 -> (6, Some(8))
-        | _ -> (lanternfish - 1, None)
+        | 0 -> [ 6; 8 ]
+        | _ -> [ lanternfish - 1 ]
 
-    let rec simulateSchool (school: Lanternfish seq) : Lanternfish list =
-        school
-        |> Seq.fold
-            (fun (oldFish: Lanternfish list, newFish: Lanternfish list) lanternfish ->
-                let fish, spawn = simulateLanternfish lanternfish
+    let simulateSchool (school: School) : School =
+        let newSchool = createSchool
 
-                (fish :: oldFish,
-                 if spawn.IsSome then
-                     spawn.Value :: newFish
-                 else
-                     newFish))
-            ([], [])
-        |> fun (oldFish, newFish) -> List.append (oldFish |> List.rev) newFish
+        for timer in [ 0 .. 8 ] do
+            let qty = school.[timer]
+            Array.set newSchool timer qty
+
+        newSchool
+
 
     let rec simulate days (school: School) =
         if days > 0 then
@@ -35,68 +33,147 @@ module Day6 =
         else
             school
 
-    let countFish (school: School) = school |> Seq.length
+    let countFish (school: School) = school |> Seq.sum
 
     let parseInput (input: string) =
-        input.Split ',' |> Seq.map Convert.ToInt32
+        let school = createSchool
+
+        input.Split ','
+        |> Seq.map Convert.ToInt32
+        |> Seq.iter (fun (fish: Lanternfish) -> Array.set school fish (school.[fish] + 1UL))
+
+        school
 
     type Tests(output: ITestOutputHelper) =
         let testDataInput = "3,4,3,1,2"
 
         let testData = testDataInput |> parseInput
 
-        [<Fact>]
-        let ``Simulates a lanternfish`` () = Assert.Equal([ 2 ], simulate 1 [ 3 ])
-
-        [<Fact>]
-        let ``Simulates a lanternfish`` () = Assert.Equal([ 1 ], simulate 2 [ 3 ])
-
-        [<Fact>]
-        let ``Simulates a lanternfish`` () = Assert.Equal([ 0 ], simulate 3 [ 3 ])
+        let SchoolsEqual (a: int array) (b: School) =
+            Assert.Equal(a |> Seq.map Convert.ToUInt64 |> Seq.toArray, b |> Seq.toList)
 
         [<Fact>]
         let ``Simulates a lanternfish`` () =
-            Assert.Equal([ 6; 8 ], simulate 4 [ 3 ])
+            SchoolsEqual [| 0
+                            0
+                            1
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0 |],
+            simulate
+                1
+                [| 0UL
+                   0UL
+                   0UL
+                   1UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL |]
 
         [<Fact>]
         let ``Simulates a lanternfish`` () =
-            Assert.Equal([ 5; 7 ], simulate 5 [ 3 ])
+            SchoolsEqual [| 0
+                            1
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0 |],
+            simulate
+                2
+                [| 0UL
+                   0UL
+                   0UL
+                   1UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL |]
 
         [<Fact>]
-        let ``Simulates a school of lanternfish`` () =
-            Assert.Equal(
-                [ 6
-                  0
-                  6
-                  4
-                  5
-                  6
-                  0
-                  1
-                  1
-                  2
-                  6
-                  0
-                  1
-                  1
-                  1
-                  2
-                  2
-                  3
-                  3
-                  4
-                  6
-                  7
-                  8
-                  8
-                  8
-                  8 ],
-                testData |> simulate 18
-            )
+        let ``Simulates a lanternfish`` () =
+            SchoolsEqual [| 1
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0
+                            0 |],
+            simulate
+                3
+                [| 0UL
+                   0UL
+                   0UL
+                   1UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL |]
+
+        [<Fact>]
+        let ``Simulates a lanternfish`` () =
+            SchoolsEqual [| 0
+                            0
+                            0
+                            0
+                            0
+                            0
+                            1
+                            0
+                            1 |],
+            simulate
+                4
+                [| 0UL
+                   0UL
+                   0UL
+                   1UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL |]
+
+        [<Fact>]
+        let ``Simulates a lanternfish`` () =
+            SchoolsEqual [| 0
+                            0
+                            0
+                            0
+                            0
+                            1
+                            0
+                            1
+                            0 |],
+            simulate
+                5
+                [| 0UL
+                   0UL
+                   0UL
+                   1UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL
+                   0UL |]
 
         [<Fact>]
         let ``Counts lanternfish`` () =
-            Assert.Equal(26, testData |> simulate 18 |> countFish)
+            Assert.Equal(26UL, testData |> simulate 18 |> countFish)
+
+        [<Fact>]
+        let ``Counts lanternfish`` () =
+            Assert.Equal(26984457539UL, testData |> simulate 256 |> countFish)
 
         [<Fact>]
         let ``Simulates lanternfish with real data`` () =
