f10cd341e749d50d34cb2ad7414d0df26eed0c45
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-07 07:46:37 +0000

    Add empty day 7

---
+++ Tests2021/Day7.fs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode
+
+open System
+open Xunit.Abstractions
+
+module Day7 =
+
+    let parseInput (input: string) =
+        input.Split ',' |> Seq.map Convert.ToInt32
+
+    type Tests(output: ITestOutputHelper) =
+        let testDataInput = "16,1,2,0,4,2,7,1,2,14"
+
+        let testData = testDataInput |> parseInput
