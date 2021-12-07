3842ce5f7e85b44cc83a641af72a7dc8678a667c
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-07 08:03:48 +0000

    Calculate fuel to move to a single position

--- Tests2021/Day7.fs
+++ Tests2021/Day7.fs
@@ -18,14 +18,31 @@ module Day7 =
         positions
         |> Seq.fold (fun (map: Map<HorizontalPosition, int>) point -> map.Change(point, increment)) Map.empty
 
-    let calculateFuelToMoveToPosition target (crabPositions: Map<HorizontalPosition, int>) = 0
+    let calculateFuelToMoveToPosition target (crabPositions: Map<HorizontalPosition, int>) =
+        crabPositions
+        |> Map.toSeq
+        |> Seq.fold (fun totalFuel (position, crabs) -> totalFuel + abs (position - target) * crabs) 0
 
     type Tests(output: ITestOutputHelper) =
         let testDataInput = "16,1,2,0,4,2,7,1,2,14"
 
-        let testData = testDataInput |> parseInput |> countCrabsAtEachPosition
+        let testData =
+            testDataInput
+            |> parseInput
+            |> countCrabsAtEachPosition
 
         [<Fact>]
         let ``Calculates fuel required to move all crabs to position 2`` () =
-            let fuel = testData |> calculateFuelToMoveToPosition 2
-            Assert.Equal(37, fuel)
+            Assert.Equal(37, (testData |> calculateFuelToMoveToPosition 2))
+
+        [<Fact>]
+        let ``Calculates fuel required to move all crabs to position 1`` () =
+            Assert.Equal(41, (testData |> calculateFuelToMoveToPosition 1))
+
+        [<Fact>]
+        let ``Calculates fuel required to move all crabs to position 3`` () =
+            Assert.Equal(39, (testData |> calculateFuelToMoveToPosition 3))
+
+        [<Fact>]
+        let ``Calculates fuel required to move all crabs to position 10`` () =
+            Assert.Equal(71, (testData |> calculateFuelToMoveToPosition 10))
