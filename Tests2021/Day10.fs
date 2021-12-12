bef624f1d986f14ee882f890b6cdffb6da8a219f
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-12 10:45:32 +0000

    Find invalid characters

--- Tests2021/Day10.fs
+++ Tests2021/Day10.fs
@@ -1,10 +1,56 @@
 ﻿module Tests2021.Day10
 
+open System.Collections.Immutable
 open FsUnit
 open Xunit
 open Xunit.Abstractions
 
-let findFirstIllegalCharacter line : char option = None
+type Pair =
+    { opener: char
+      closer: char
+      score: int }
+
+let legalPairs: Pair list =
+    [ { opener = '('
+        closer = ')'
+        score = 3 }
+      { opener = '['
+        closer = ']'
+        score = 57 }
+      { opener = '{'
+        closer = '}'
+        score = 1197 }
+      { opener = '<'
+        closer = '>'
+        score = 25137 } ]
+
+let isOpener c =
+    legalPairs |> Seq.exists (fun p -> p.opener = c)
+
+let findOpener c =
+    legalPairs |> Seq.tryFind (fun p -> p.opener = c)
+
+
+let processCharacter (openChunks: ImmutableStack<Pair>, illegalCharacter: char option) (character: char) =
+    if illegalCharacter.IsSome then
+        (openChunks, illegalCharacter)
+    else
+        let pair = findOpener character
+
+        if pair.IsSome then
+            (openChunks.Push(pair.Value), None)
+        else
+            let topChunk = openChunks.Peek()
+
+            if topChunk.closer = character then
+                (openChunks.Pop(), None)
+            else
+                (openChunks, Some character)
+
+let findFirstIllegalCharacter line : char option =
+    line
+    |> Seq.fold processCharacter (ImmutableStack<Pair>.Empty, None)
+    |> snd
 
 type Tests(output: ITestOutputHelper) =
 
@@ -45,8 +91,8 @@ type Tests(output: ITestOutputHelper) =
 
     [<Fact>]
     let ``Recognises corrupted lines`` () =
-        exampleValidChunks
+        exampleCorruptedChunks
         |> Seq.map findFirstIllegalCharacter
         |> Seq.toList
         |> List.map (fun (x: char option) -> x.IsSome)
-        |> should equal (List.init exampleValidChunks.Length (fun _ -> true))
+        |> should equal (List.init exampleCorruptedChunks.Length (fun _ -> true))
