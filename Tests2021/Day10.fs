c30883d3ec6fcbb290841967d948369f8af5f901
Graeme Foster <graeme@codeforbreakfast.co>
2021-12-12 09:50:49 +0000

    Start day 10

---
+++ Tests2021/Day10.fs
@@ -0,0 +1,52 @@
+﻿module Tests2021.Day10
+
+open FsUnit
+open Xunit
+open Xunit.Abstractions
+
+let findFirstIllegalCharacter line : char option = None
+
+type Tests(output: ITestOutputHelper) =
+
+    let exampleValidChunks =
+        [ "()"
+          "[]"
+          "([])"
+          "{()()()}"
+          "<([{}])>"
+          "[<>({}){}[([])<>]]"
+          "(((((((((())))))))))" ]
+
+    let exampleCorruptedChunks =
+        [ "(]"
+          "{()()()>"
+          "(((()))}"
+          "<([]){()}[{}])" ]
+
+    let example =
+        "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"
+
+    [<Fact>]
+    let ``Recognises valid chunks`` () =
+        exampleValidChunks
+        |> Seq.map findFirstIllegalCharacter
+        |> Seq.toList
+        |> List.map (fun x -> x.IsNone)
+        |> should equal (List.init exampleValidChunks.Length (fun _ -> true))
+
+    [<Fact>]
+    let ``Recognises corrupted lines`` () =
+        exampleValidChunks
+        |> Seq.map findFirstIllegalCharacter
+        |> Seq.toList
+        |> List.map (fun (x: char option) -> x.IsSome)
+        |> should equal (List.init exampleValidChunks.Length (fun _ -> true))
